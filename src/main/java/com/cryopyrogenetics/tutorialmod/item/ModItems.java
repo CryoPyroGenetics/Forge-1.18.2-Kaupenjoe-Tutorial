befc197f72bf423e0ba059bd1840baf0bd7887c0
Zachary Taylor <cryopyrogenetics@gmail.com>
2023-02-24 13:15:00 -0500

    Added Cucumber Food Item

--- src/main/java/com/cryopyrogenetics/tutorialmod/item/ModItems.java
+++ src/main/java/com/cryopyrogenetics/tutorialmod/item/ModItems.java
@@ -21,6 +21,9 @@ public class ModItems {
     public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
             () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).durability(64)));
 
+    public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).food(ModFoods.CUCUMBER)));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
