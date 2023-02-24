bb12cc2a76a58149f8394a792dcdd41356c8fdf5
Zachary Taylor <cryopyrogenetics@gmail.com>
2023-02-24 13:45:12 -0500

    Made Speed Blocks change model based on what direction they are placed.

--- src/main/java/com/cryopyrogenetics/tutorialmod/block/custom/SpeedyBlock.java
+++ src/main/java/com/cryopyrogenetics/tutorialmod/block/custom/SpeedyBlock.java
@@ -5,14 +5,25 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import org.jetbrains.annotations.NotNull;
 
-public class SpeedyBlock extends Block {
+public class SpeedyBlock extends HorizontalDirectionalBlock {
     public SpeedyBlock(Properties pProperties) {
         super(pProperties);
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
     }
 
     @Override
