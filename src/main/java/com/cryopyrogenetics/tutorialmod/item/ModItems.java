9d31bbe84c16b91bac8418372fcc23a1e474f9aa
Zachary Taylor <cryopyrogenetics@gmail.com>
2023-02-26 19:01:01 -0500

    Added Citrine Sword, Pickaxe, Axe, Shovel, and Hoe.

--- src/main/java/com/cryopyrogenetics/tutorialmod/item/ModItems.java
+++ src/main/java/com/cryopyrogenetics/tutorialmod/item/ModItems.java
@@ -3,7 +3,7 @@ package com.cryopyrogenetics.tutorialmod.item;
 import com.cryopyrogenetics.tutorialmod.TutorialMod;
 import com.cryopyrogenetics.tutorialmod.item.custom.CoalCokeItem;
 import com.cryopyrogenetics.tutorialmod.item.custom.DowsingRodItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -27,6 +27,26 @@ public class ModItems {
 
     public static final RegistryObject<Item> COAL_COKE = ITEMS.register("coal_coke",
             () -> new CoalCokeItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_SWORD = ITEMS.register("citrine_sword",
+            () -> new SwordItem(ModTiers.CITRINE, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_PICKAXE = ITEMS.register("citrine_pickaxe",
+            () -> new PickaxeItem(ModTiers.CITRINE, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_AXE = ITEMS.register("citrine_axe",
+            () -> new AxeItem(ModTiers.CITRINE, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_SHOVEL = ITEMS.register("citrine_shovel",
+            () -> new ShovelItem(ModTiers.CITRINE, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_HOE = ITEMS.register("citrine_hoe",
+            () -> new HoeItem(ModTiers.CITRINE, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
 
     public static void register(IEventBus eventBus) {
